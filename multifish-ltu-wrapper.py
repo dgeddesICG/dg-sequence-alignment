d19129499db90e29eac009a849d2f4dae606c63f
Daniel Geddes <daniel.geddes@glasgow.ac.uk>
2026-05-28 13:54:40 +0100

    Changes to the multifish wrapper that means I don't pass token values back to the Objective-C side needlessly. These function now just don't return anything which manifests as a python None type satisfying the check Is_PyNone() in the Objective C side.

--- multifish-ltu-wrapper.py
+++ multifish-ltu-wrapper.py
@@ -45,19 +45,16 @@ def addFishToOracle(fishIndex):
 def removeFishFromOracle(fishIndex):
     # removing a fish from the oracle removes the entry at that key but also decrements the key number by one to match the behaviour of the spimGUI obj C side
     # im mostly just going to copy the logic of the SpimApplication.removeFish method
-    returnFlag = 1
     fishIndices = sorted(keys for keys in multifishOracle.keys())
     maxFishIndex = max(fishIndices)
     numFishInOracle = len(fishIndices)
     if (isFishProfileInOracle(fishIndex) == False):
         print(f"Fish index {fishIndex} is not in oracle.")
-        returnFlag = 0
     elif (numFishInOracle == 1):
-        # there is only one fish in the orcale. If all rules have been followed this will be index 0
+        # there is only one fish in the oracle. If all rules have been followed this will be index 0
         print(f"We can't delete the only entry in the oracle. Resetting the LTU parameters instead for fish index {fishIndex}")
         assert(fishIndex == 0)
         updateLTUParameters([],[],[],[], fishIndex)
-        returnFlag = 0
     elif (fishIndex == maxFishIndex):
         # if its the final entry we want to remove just delete it
         del multifishOracle[fishIndex]
@@ -68,9 +65,6 @@ def removeFishFromOracle(fishIndex):
             updateLTUParameters(k,*getLTUParamters(k+1))
         # delete final entry because there is no successor to update LTUparameters from.
         del multifishOracle[maxFishIndex]
-    # return if deletion was successful
-    return returnFlag
-
 
 def updateLTUParameters(resampledSequences, periodHistory, driftHistory,  shifts, fishIndex = 0):
     if(isFishProfileInOracle(fishIndex) == True):
@@ -99,7 +93,7 @@ def getLTUParamters(fishIndex):
 # Wrapper functions for the MemoryCC module
 # ===================================================================================
 '''
-These functions call their respective functions in the memoryCC for aligning reference sequences for timelapse imaging with 
+These functions call their respective functions in memoryCC for aligning reference sequences for timelapse imaging with 
 the addition of interfacing with the multifish oracle. Each of these functions follows a common pattern on being called: get the LTUparameters from the oracle;
 combine with new data coming in from the Obj C side and pass to the old MemoryCC functions; update LTUparameters in the oracle; return required parameters back to the Obj C side.
 '''
@@ -114,14 +108,8 @@ def processNewReferenceSequence(rawFrames, thisPeriod, thisDrift,  knownPhaseInd
 
 def trimLTUHistory(trimToLength, fishIndex = 0):
     ltuParameters = getLTUParamters(fishIndex)
-
     returnTuple = mcc.trimLTUHistory(*ltuParameters, trimToLength)
     updateLTUParameters(*returnTuple,fishIndex)
-    # in this new paradigm we don't actually return anything to the obj c side. 
-    # So instead I'll compute hashes of the tuple before and after we pass it into the trimLTUHistory function.
-    # What I want is to return 0 if we've not changed anything. trimToLength is -1, or the number of reference sequences in the history.
-    # If the hashes are different then I'd presume we've updated the reference history successfully and return 1.
-    return (hash(ltuParameters) != hash(returnTuple))
 
 def RoIForReferenceHistory(fishIndex = 0):
     # this function only needs a reference to resampledSequences so I'm not going to call the updater
@@ -151,17 +139,14 @@ def numRefFrameSetsInHistory(fishIndex = 0):
 def resetRefFrameHistory(fishIndex = 0):
     # set the parameters for that entry in the oracle to an empty list
     updateLTUParameters([],[],[],[], fishIndex)
-    return 1
 
 def resetMultifishOracle():
     # set parameters for each entry in the oracle to an empty list but NOT delete them
     for keys in multifishOracle.keys():
         resetRefFrameHistory(keys)
-    return 1
 
 def clearMultifishOracle():
     # delete every entry in the oracle EXCPEPT the "0" entry. 
     # The parameters for this entry are just reset to empty lists.
     for keys in multifishOracle.keys():
         removeFishFromOracle(keys)
-    return 1
